2c23fca0465ba1905849cac8ff386887dbb18f83
marek <marek1990kaminski@gmail.com>
2017-10-15 08:48:21 +0200

    next the global object

--- app/js/src/scripts.js
+++ app/js/src/scripts.js
@@ -2,16 +2,8 @@
 var $question = document.getElementById("question");
 var $score = document.getElementById("score");
 var $feedback = document.getElementById("feedback");
-
-/// view functions ///
-function update(element, content, klass) {
-    var p = element.firstChild || document.createElement("p");
-    p.textContent = content;
-    element.appendChild(p);
-    if (klass) {
-        p.className = klass;
-    }
-}
+var $start = document.getElementById("start");
+var $form = document.getElementById("answer");
 
 var quiz = {
     "name": "Super Hero Name Quiz",
@@ -24,17 +16,48 @@ var quiz = {
     ]
 };
 
-
 var score = 0; // initialize score
 
-update($score, score);
+/// view functions ///
+function update(element, content, klass) {
+    var p = element.firstChild || document.createElement("p");
+    p.textContent = content;
+    element.appendChild(p);
+    if (klass) {
+        p.className = klass;
+    }
+}
 
+//helper functions
+function hide(element) {
+    element.style.display = "none";
+}
+
+function show(element) {
+    element.style.display = "block";
+}
+
+
+// hide the form at the start of the game
+hide($form);
+
+//play function
 function play(quiz) {
+
+    // hide button and show form
+    hide($start);
+    show($form);
+
+    $form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        check($form[0].value);
+    }, false);
 
     //functions declarations
     function ask(question) {
         update($question, quiz.question + question);
-        return prompt(quiz.question + question);
+        $form[0].value = "";
+        $form[0].focus();
     }
 
     function check(answer) {
@@ -46,25 +69,36 @@ function play(quiz) {
         } else {
             update($feedback, "Wrong!", "wrong");
         }
+
+        i++;
+        if (i === quiz.questions.length) {
+            gameOver();
+        } else {
+            chooseQuestion();
+        }
+    }
+
+    function chooseQuestion() {
+        var question = quiz.questions[i].question;
+        ask(question);
     }
 
     function gameOver() {
         // inform the player that the game has finished and tell them how many points they have scored
         update($question, "Game Over, you scored " + score + " points");
+
+        hide($form);
+        show($start);
     }
 
-    // main game loop
-    for (var i = 0, question, answer, max = quiz.questions.length; i < max; i++) {
-        question = quiz.questions[i].question;
-        answer = ask(question);
-        check(answer);
-    }
-    // end of main game loop
-
-    gameOver();
+    var i = 0;
+    chooseQuestion();
 
 }
 
+// Event listeners
+$start.addEventListener('click', function () {
+    play(quiz);
+}, false);
 
-//finally use this glamorous function
-play(quiz);
+update($score, score);
