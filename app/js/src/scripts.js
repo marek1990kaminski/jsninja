4a24fea26a2077dd7e9d232d86dbe865bb7387b6
Marek K <marekkaminski26@gmail.com>
2017-10-21 17:03:20 +0200

    some console logs added... also I have imrove the form a little bit

--- app/js/src/scripts.js
+++ app/js/src/scripts.js
@@ -1,126 +1,143 @@
+(function () {
+    "use strict";
 //// dom references ////
-var $question = document.getElementById("question");
-var $score = document.getElementById("score");
-var $feedback = document.getElementById("feedback");
-var $start = document.getElementById("start");
-var $form = document.getElementById("answer");
-var $timer = document.getElementById("timer");
+    var $question = document.getElementById("question");
+    var $score = document.getElementById("score");
+    var $feedback = document.getElementById("feedback");
+    var $start = document.getElementById("start");
+    var $form = document.getElementById("answer");
+    var $timer = document.getElementById("timer");
 
-var quiz = {
-    "name": "Super Hero Name Quiz",
-    "description": "How many super heroes can you name?",
-    "question": "What is the real name of ",
-    "questions": [
-        {"question": "Superman", "answer": "Clarke Kent"},
-        {"question": "Batman", "answer": "Bruce Wayne"},
-        {"question": "Wonder Woman", "answer": "Dianna Prince"}
-    ]
-};
+    var quiz = {
+        "name": "Super Hero Name Quiz",
+        "description": "How many super heroes can you name?",
+        "question": "What is the real name of ",
+        "questions": [
+            {"question": "Superman", "answer": "Clarke Kent"},
+            {"question": "Batman", "answer": "Bruce Wayne"},
+            {"question": "Wonder Woman", "answer": "Dianna Prince"}
+        ]
+    };
 
-var score = 0; // initialize score
+    var score = 0; // initialize score
 
 /// view functions ///
-function update(element, content, klass) {
-    var p = element.firstChild || document.createElement("p"); //firstChild works, because every element in HTM file has been created without any whitespaces inside
-    p.textContent = content;
-    element.appendChild(p);
-    if (klass) {
-        p.className = klass;
+    function update(element, content, klass) {
+        console.trace();
+        var p = element.firstChild || document.createElement("p"); //firstChild works, because every element in HTM file has been created without any whitespaces inside
+        p.textContent = content;
+        element.appendChild(p);
+        if (klass) {
+            p.className = klass;
+        }
     }
-}
 
 //helper functions
-function hide(element) {
-    element.style.display = "none";
-}
+    function hide(element) {
+        element.style.display = "none";
+    }
 
-function show(element) {
-    element.style.display = "block";
-}
+    function show(element) {
+        element.style.display = "block";
+    }
 
 
 // hide the form at the start of the game
-hide($form);
+    hide($form);
 
 //play function
-function play(quiz) {
+    function play(quiz) {
 
-    // hide button and show form
-    hide($start);
-    show($form);
+        // hide button, and show form and feedback, besides, feedback needs to be cleared.
+        hide($start);
+        $feedback.innerHTML = "";
+        show($feedback);
+        show($form);
 
-    // initialize timer and set up an interval that counts down
-    var time = 20;
-    update($timer, time);
-    var interval = window.setInterval(countDown, 1000);
+        //reset score
+        score = 0;
+        update($score, score);
 
-    $form.addEventListener('submit', function (event) {
-        event.preventDefault();
-        check($form[0].value);
-    }, false);
-
-    //functions declarations
-
-    // this is called every second and decreases the time
-    function countDown() {
-        // decrease time by 1
-        time--;
-        // update the time displayed
+        // initialize timer and set up an interval that counts down
+        var time = 20;
         update($timer, time);
-        // the game is over if the timer has reached 0
-        if (time <= 0) {
-            gameOver();
+        var interval = window.setInterval(countDown, 1000);
+
+        $form.addEventListener('submit', function (event) {
+            event.preventDefault();
+            check($form[0].value);
+        }, false);
+
+        //functions declarations
+
+        // this is called every second and decreases the time
+        function countDown() {
+            // decrease time by 1
+            time--;
+            // update the time displayed
+            update($timer, time);
+            // the game is over if the timer has reached 0
+            if (time <= 0) {
+                gameOver();
+            }
         }
-    }
 
-    function ask(question) {
-        update($question, quiz.question + question);
-        $form[0].value = "";
-        $form[0].focus();
-    }
-
-    function check(answer) {
-        if (answer === quiz.questions[i].answer) { // quiz[i][1] is the ith answer
-            update($feedback, "Correct!", "right");
-            // increase score by 1
-            score++;
-            update($score, score)
-        } else {
-            update($feedback, "Wrong!", "wrong");
+        function ask(question) {
+            console.log("ask() invoked");
+            update($question, quiz.question + question);
+            $form[0].value = "";
+            $form[0].focus();
         }
 
-        i++;
-        if (i === quiz.questions.length) {
-            gameOver();
-        } else {
-            chooseQuestion();
+        function check(answer) {
+            console.log("check() invoked");
+
+            if (answer === quiz.questions[i].answer) { // quiz[i][1] is the ith answer
+                update($feedback, "Correct!", "right");
+                // increase score by 1
+                score++;
+                update($score, score)
+            } else {
+                update($feedback, "Wrong!", "wrong");
+            }
+
+            i++;
+            if (i === quiz.questions.length) {
+                gameOver();
+            } else {
+                chooseQuestion();
+            }
         }
+
+        function chooseQuestion() {
+            console.log("chooseQuestion() invoked");
+            var question = quiz.questions[i].question;
+            ask(question);
+        }
+
+        function gameOver() {
+            console.log("gameOver() invoked");
+            // inform the player that the game has finished and tell them how many points they have scored
+            update($question, "Game Over, you scored " + score + " points");
+
+            hide($form);
+            hide($feedback);
+            show($start);
+
+            // stop the countdown interval
+            window.clearInterval(interval);
+        }
+
+        var i = 0;
+        chooseQuestion();
+
     }
-
-    function chooseQuestion() {
-        var question = quiz.questions[i].question;
-        ask(question);
-    }
-
-    function gameOver() {
-        // inform the player that the game has finished and tell them how many points they have scored
-        update($question, "Game Over, you scored " + score + " points");
-
-        hide($form);
-        show($start);
-
-        // stop the countdown interval
-        window.clearInterval(interval);
-    }
-
-    var i = 0;
-    chooseQuestion();
-
-}
 
 // Event listeners
-$start.addEventListener('click', function () {
-    play(quiz);
-}, false);
+    $start.addEventListener('click', function () {
+        play(quiz);
+    }, false);
 
-update($score, score);
+    update($score, score);
+
+}());
