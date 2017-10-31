056b2aeebdd383273d25d18e210209f238dafb0b
Marek K <marekkaminski26@gmail.com>
2017-10-31 16:07:31 +0100

    now with a class Game. Stumbled upon an event listener problem

--- app/js/src/scripts.js
+++ app/js/src/scripts.js
@@ -1,5 +1,5 @@
 (function () {
-    "use strict";
+    "use strict"
 //// dom references ////
 
     var $question = document.getElementById("question");
@@ -9,10 +9,6 @@
     var $form = document.getElementById("answer");
     var $timer = document.getElementById("timer");
 
-//stop the automatic refresh of page when the form is submitted.
-    $form.addEventListener("submit", function (e) {
-        e.preventDefault();
-    });
 
     var quiz = {
         "name": "Super Hero Name Quiz",
@@ -28,9 +24,9 @@
     };
 
     var question; // current question
-    var score = 0; // initialize score
+    var initialScore = 0; // initialize score
 
-    var i = 0; //initialize counter
+    var i = 0; //initialize counter of questions... maybe we can make it so it isn't global variable
 /// view functions ///
     function update(element, content, klass) {
         console.trace();
@@ -74,146 +70,155 @@
     }
 
 //play function
-    function play(quiz) {
+    function Game(quiz) {
+        //first lets set some properties
+        this.questions = quiz.questions;
+        this.phrase = quiz.question;
+        this.score = 0;//initialize score
+        update($score, this.score); //and make it visible
+
+        // initialize timer and set up an interval that counts down
+        this.time = 20;
+        update($timer, this.time);
+        this.interval = window.setInterval(this.countDown.bind(this), 1000);
 
         // hide button, and show form and feedback and timer, besides, feedback needs to be cleared.
         hide($start);
-        $feedback.innerHTML = "";
         show($form);
-        show($feedback);
         show($timer);
+        $feedback.innerHTML = "";
+        show($feedback);
 
-
-        //reset score
-        score = 0;
-        update($score, score);
-
-        // initialize timer and set up an interval that counts down
-        var time = 20;
-        update($timer, time);
-        var interval = window.setInterval(countDown, 1000);
 
         // add event listener to form for when it's submitted
-        function onClickFireCheck(e) {
-            check(e.target.value);
-        }
 
-        $form.addEventListener('click', onClickFireCheck, false);
+        $form.addEventListener('click', this.onClickFireCheck.bind(this), false);
 
-        //functions declarations
-
-        // this is called every second and decreases the time
-        function countDown() {
-            // decrease time by 1
-            time--;
-            // update the time displayed
-            update($timer, time);
-            // the game is over if the timer has reached 0
-            if (time <= 0) {
-                gameOver();
-            }
-        }
-
-        function ask(question) {
-            console.log("ask() invoked");
-            // set the question.asked property to true so it's not asked again
-            question.asked = true;
-            update($question, quiz.question + question.question + '?');
-            // create an array to put the different options in and a button variable
-            var options = [], button;
-            var option1 = chooseOption();
-            options.push(option1.answer);
-            var option2 = chooseOption();
-            options.push(option2.answer);
-            // add the actual answer at a random place in the options array
-            options.splice(random(0, 2), 0, question.answer);
-
-            // loop through each option and display it as a button
-            options.forEach(function (name) {
-                button = document.createElement("button");
-                button.value = name;
-                button.textContent = name;
-                $form.appendChild(button);
-            });
-
-            // choose an option from all the possible answers but without choosing the same option twice
-            function chooseOption() {
-                var option = quiz.questions[random(quiz.questions.length) - 1];
-                // check to see if the option chosen is the current question or already one of the options, if it is then recursively call this function until it isn't
-                if (option === question || options.indexOf(option.answer) !== -1) {
-                    return chooseOption();
-                }
-                return option;
-            }
-        }
-
-        function check(answer) {
-            console.log("check() invoked");
-            //ternaries
-            answer === question.answer ? (
-                    update($feedback, "Correct!", "right"),
-                        // increase score by 1
-                        score++,
-                        update($score, score))
-                : (
-                    update($feedback, "Wrong!", "wrong")
-                )
-            i++;
-            //now how the hell do i check if thats last answer?
-            (i === quiz.questions.length) ? (
-                gameOver()
-            ) : (
-                chooseQuestion()
-            )
-        }
-
-        function chooseQuestion() {
-            console.log("chooseQuestion() invoked");
-            var questions = quiz.questions.filter(function (question) {
-                return question.asked === false;
-            });
-
-            //NOW CLEAR THE FUCKING FORM, YO...
-            $form.innerHTML = "";
-            // set the current question
-            question = questions[random(questions.length) - 1];
-            ask(question);
-        }
-
-        function gameOver() {
-            console.log("gameOver() invoked");
-            // inform the player that the game has finished and tell them how many points they have scored
-            update($question, "Game Over, you scored " + score + " points");
-
-            hide($form);
-            hide($timer);
-            show($start);
-
-            // stop the countdown interval
-            window.clearInterval(interval);
-
-            //reset questions counter
-            i = 0;
-
-            quiz.questions.forEach(function (question) {
-                question.asked = false;
-            });
-
-            //remove event listener from $form
-            $form.removeEventListener("click", onClickFireCheck, false);
-        }
-
-        chooseQuestion();
-
+        this.chooseQuestion();
     }
+
+    //here we fumble in the Game.prototype
+    Game.prototype.chooseQuestion = function () {
+        console.log("chooseQuestion() invoked");
+        var questions = this.questions.filter(function (question) {
+            return question.asked === false;
+        });
+
+        // set the current question
+        this.question = questions[random(questions.length) - 1];
+        this.ask(this.question);
+    }
+
+    Game.prototype.ask = function (question) {
+        console.log("ask() invoked");
+
+        var quiz = this;
+
+        // set the question.asked property to true so it's not asked again
+        question.asked = true;
+        update($question, this.phrase + question.question + '?');
+
+        //NOW CLEAR THE FUCKING FORM, YO...
+        $form.innerHTML = "";
+
+        // create an array to put the different options in and a button variable
+        var options = [], button;
+        var option1 = chooseOption();
+        options.push(option1.answer);
+        var option2 = chooseOption();
+        options.push(option2.answer);
+        // add the actual answer at a random place in the options array
+        options.splice(random(0, 2), 0, question.answer);
+
+        // loop through each option and display it as a button
+        options.forEach(function (name) {
+            button = document.createElement("button");
+            button.value = name;
+            button.textContent = name;
+            $form.appendChild(button);
+        });
+
+        // choose an option from all the possible answers but without choosing the same option twice. We have to choose one of the objects randomly, to find proposition for answer that lays within.
+        function chooseOption() {
+            var option = quiz.questions[random(quiz.questions.length) - 1];
+            // check to see if the option chosen is the current question or already one of the options, if it is then recursively call this function until it isn't
+            if (option === question || options.indexOf(option.answer) !== -1) {
+                return chooseOption();
+            }
+            return option;
+        }
+    }
+
+    Game.prototype.check = function (answer) {
+        console.log("check() invoked");
+        //ternaries
+        answer === this.question.answer ? (
+                update($feedback, "Correct!", "right"),
+                    // increase score by 1
+                    this.score++,
+                    update($score, this.score))
+            : (
+                update($feedback, "Wrong!", "wrong")
+            )
+        i++;
+        //now how the hell do I check if thats last answer?
+        (i === quiz.questions.length) ? (
+            this.gameOver()
+        ) : (
+            this.chooseQuestion()
+        )
+    }
+
+    Game.prototype.countDown = function () {
+        // decrease time by 1
+        this.time--;
+        // update the time displayed
+        update($timer, this.time);
+        // the game is over if the timer has reached 0
+        if (this.time <= 0) {
+            this.gameOver();
+        }
+    }
+
+    Game.prototype.onClickFireCheck = function (e) {
+        this.check(e.target.value);
+    }
+
+    Game.prototype.gameOver = function(){
+        console.log("gameOver() invoked");
+        // inform the player that the game has finished and tell them how many points they have scored
+        update($question, "Game Over, you scored " + this.score + " points");
+
+        hide($form);
+        hide($timer);
+        show($start);
+
+        // stop the countdown interval
+        window.clearInterval(this.interval);
+
+        //reset questions counter
+        i = 0;
+
+        this.questions.forEach(function (question) {
+            question.asked = false;
+        });
+
+        //remove event listener from $form
+        $form.removeEventListener("click", this.onClickFireCheck.bind(this), false);
+    }
+
 
 // Event listeners
     $start.addEventListener('click', function () {
-        play(quiz);
+        new Game(quiz);
     }, false);
 
-    update($score, score);
+    update($score, initialScore);
 
+    //stop the automatic refresh of page when the form is submitted.
+    $form.addEventListener("submit", function (e) {
+        e.preventDefault();
+    }, false);
 
 }());
 
